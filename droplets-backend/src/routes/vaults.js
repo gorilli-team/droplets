2c68da5f1e501dfbd0cbb26a43da3cc42401adbf
Christian Kobril <christiankobril@gmail.com>
2024-11-08 16:24:24 +0100

    feat: create a new vault and save on db

--- droplets-backend/src/routes/vaults.js
+++ droplets-backend/src/routes/vaults.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Vault = require('../models/vault');
+const Vault = require("../models/vault");
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const vault = await Vault.findById(req.params.id);
     res.send(vault);
@@ -13,9 +13,17 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const vaults = await Vault.find();
+    const { ownerAddress } = req.query;
+    let vaults;
+
+    if (ownerAddress) {
+      vaults = await Vault.find({ ownerAddress });
+    } else {
+      vaults = await Vault.find();
+    }
+
     res.send({ vaults, count: vaults.length });
   } catch (error) {
     res.status(500).send({
@@ -24,9 +32,9 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    console.log('Adding Vault');
+    console.log("Adding Vault");
     const vault = new Vault(req.body);
     await vault.save();
 
@@ -38,9 +46,11 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
-    const vault = await Vault.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const vault = await Vault.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
     res.send(vault);
   } catch (error) {
     res.status(500).send({
