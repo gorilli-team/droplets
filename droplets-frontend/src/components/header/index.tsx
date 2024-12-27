53b66d28c0c7489707632cd6384b1c7642caa8da
Paolo Antonio Rossi <paoloantoniorossi@gmail.com>
2024-12-27 21:12:34 +0100

    Update index.tsx

--- droplets-frontend/src/components/header/index.tsx
+++ droplets-frontend/src/components/header/index.tsx
@@ -1,17 +1,17 @@
-import logo from "../src/assets/images/logo.png";
-import { ConnectButton } from "@particle-network/connectkit";
-import Image from "next/image";
-import Link from "next/link";
+import logo from '../src/assets/images/logo.png';
+import { ConnectButton } from '@particle-network/connectkit';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import styles from "./index.module.css";
+import styles from './index.module.css';
 
 export default function Header() {
   return (
     <header className="bg-gradient-to-r from-teal-500 to-blue-600 p-2 text-white flex items-center justify-between">
-      <Link href="/">
-        <img src="/img/gorilli.png" alt="Droplets Logo" className="h-16" />{" "}
+      <Link href="https://droplets.finance">
+        <img src="/img/gorilli.png" alt="Droplets Logo" className="h-16" />{' '}
       </Link>
-      <div className={styles["nav-end"]}>
+      <div className={styles['nav-end']}>
         <ConnectButton />
       </div>
     </header>
